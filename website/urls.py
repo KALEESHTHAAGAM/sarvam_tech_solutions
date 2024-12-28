d23bb69ae277dcfd52f54b9a3a8a2580d243076c
KALEESHTHAAGM <135509097+KALEESHTHAAGAM@users.noreply.github.com>
2024-12-28 21:10:25 +0530

    update

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'), 
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('service_details/', views.service_details, name='service_details'),
+    path('projects/', views.projects, name='projects'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+def about(request):
+    return render(request, "about.html")
+def services(request):
+    return render(request, "service.html")
+def service_details(request):
+    return render(request, "service-details.html")
+def projects(request):
+    return render(request, "project.html")
+   
+def blog(request):
+    return render(request, "news-standard.html")
+def contact(request):
+    return render(request, "contact.html")
